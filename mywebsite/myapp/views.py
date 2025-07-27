8e84662cb0587522ad3e016d17193c35a944ad5b
Ivan65933 <ivan.miloserdnyi2011@gmail.com>
2025-07-28 00:13:39 +0300

    DJANGO second part

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+import calendar 
+from calendar import HTMLCalendar
+from datetime import datetime
 
-def home(request):
-    return render(request, 'home.html', {})
+def home(request, year, month):
+    name = "Ivan"
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    now = datetime.now()
+    current_year = now.year
+
+    time = now.strftime('%I:%M %p')
+
+    return render(request, 
+        'home.html', {
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time,
+        })
